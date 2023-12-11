3b752756cb45eb0a9dff7acfe34de02998fc9e2d
rafelafrance <raphael.lafrance@gmail.com>
2023-12-11 15:48:18 -0500

    Update the reconciler

--- reconcile/reconcile.py
+++ reconcile/reconcile.py
@@ -6,18 +6,109 @@ import os
 import sys
 import textwrap
 from collections import defaultdict
+from dataclasses import dataclass
+from dataclasses import field
 from pathlib import Path
 from pprint import pp
 from typing import Any
 
 import traiter.pylib.darwin_core as t_dwc
+from flora.pylib.reconcilers.admin_unit import AdminUnit
+from flora.pylib.reconcilers.id_number import IdNumber
+from flora.pylib.reconcilers.job import Job
+from flora.pylib.reconcilers.locality import Locality
+from flora.pylib.reconcilers.sex import Sex
+from flora.pylib.reconcilers.taxon_assoc import TaxonAssociation
+from flora.pylib.reconcilers.taxon_auth import TaxonAuthority
+from flora.pylib.reconcilers.taxon_name import TaxonName
+from flora.pylib.reconcilers.taxon_rank import TaxonRank
 from pylib import log
 from traiter.pylib import util as t_util
-from traiter.pylib.reconcilers.base import TEMPLATE
-from traiter.pylib.reconcilers.date_ import Date
-from traiter.pylib.reconcilers.elevation import Elevation
+from traiter.pylib.reconcilers.base import Template
+from traiter.pylib.reconcilers.coordinate_precision import CoordinatePrecision
+from traiter.pylib.reconcilers.coordinate_uncertainty import CoordinateUncertainty
+from traiter.pylib.reconcilers.decimal_latitude import DecimalLatitude
+from traiter.pylib.reconcilers.decimal_longitude import DecimalLongitude
+from traiter.pylib.reconcilers.event_date import EventDate
+from traiter.pylib.reconcilers.geodetic_datum import GeodeticDatum
 from traiter.pylib.reconcilers.habitat import Habitat
-from traiter.pylib.reconcilers.lat_long import LatLong
+from traiter.pylib.reconcilers.maximum_elevation import MaximumElevationInMeters
+from traiter.pylib.reconcilers.minimum_elevation import MinimumElevationInMeters
+from traiter.pylib.reconcilers.verbatim_coordinates import VerbatimCoordinates
+from traiter.pylib.reconcilers.verbatim_elevation import VerbatimElevation
+from traiter.pylib.reconcilers.verbatim_system import VerbatimCoordinateSystem
+
+
+@dataclass
+class Row:
+    stem: str
+    text: str = ""
+    traiter: dict[str, Any] = field(default_factory=dict)
+    openai: dict[str, Any] = field(default_factory=dict)
+    reconciled: dict[str, Any] = field(default_factory=dict)
+    missed: set[str] = field(default_factory=set)
+    errors: list[str] = field(default_factory=list)
+
+    def get_text(self, text_dir):
+        path = text_dir / f"{self.stem}.txt"
+        with open(path) as f:
+            text = f.read()
+        self.text = t_util.compress(text)
+
+    def get_traiter(self, traiter_dir):
+        path = traiter_dir / f"{self.stem}.json"
+        with open(path) as f:
+            self.traiter = json.load(f)
+
+    def get_openai(self, openai_dir):
+        path = openai_dir / f"{self.stem}.json"
+        with open(path) as f:
+            openai = json.load(f)
+
+        new = {}
+        for key, value in openai.items():
+            new[key] = value
+            if isinstance(value, dict):
+                new |= {k: v for k, v in value.items()}
+
+        self.openai = {clean_key(k): v for k, v in new.items()}
+
+    def reconcile(self, template):
+        for func in template.actions:
+            try:
+                self.reconciled |= func(self.traiter, self.openai)
+            except ValueError as err:
+                self.errors.append(str(err))
+
+        self.missed = set(self.openai.keys()) - set(self.reconciled.keys())
+
+    def save_traits(self, parsed_dir):
+        path = parsed_dir / f"{self.stem}.json"
+        with open(path, "w") as f:
+            json.dump(self.reconciled, f)
+
+    def verbose_start(self):
+        print("=" * 80)
+        print(self.stem)
+        print()
+
+    def verbose(self):
+        if self.errors:
+            print()
+        print(f"Errors: {self.errors}")
+        print()
+        print("---- Text ", "-" * 40)
+        print(self.text)
+        print()
+        print("---- Traiter ", "-" * 40)
+        pp(self.traiter)
+        print()
+        print("---- OpenAI ", "-" * 40)
+        pp(self.openai)
+        print()
+        print("---- Reconciled ", "-" * 40)
+        pp(self.reconciled)
+        print()
 
 
 def main():
@@ -42,108 +133,70 @@ def main():
     )
 
     if args.count:
-        count_keys(args.openai_dir, openai_stems)
+        count_keys(args.openai_dir)
 
     else:
-        build_template()
+        template = Template(
+            EventDate,
+            MinimumElevationInMeters,
+            MaximumElevationInMeters,
+            VerbatimElevation,
+            Habitat,
+            DecimalLatitude,
+            DecimalLongitude,
+            CoordinateUncertainty,
+            CoordinatePrecision,
+            GeodeticDatum,
+            VerbatimCoordinateSystem,
+            VerbatimCoordinates,
+            AdminUnit,
+            IdNumber,
+            Locality,
+            Sex,
+            Job,
+            TaxonAssociation,
+            TaxonAuthority,
+            TaxonName,
+            TaxonRank,
+        )
+
+        total_errors = 0
+        rows = []
 
         for stem in stems:
-            text = get_text(args.text_dir, stem)
-            rule_traits = get_rule_traits(args.traiter_dir, stem)
-            openai_traits = get_openai_traits(args.openai_dir, stem)
+            row = Row(stem=stem)
+            rows.append(row)
 
-            traits, errors = get_traits(text, stem, rule_traits, openai_traits)
-            if errors > args.errors:
-                logging.error(f"Total errors: {errors}")
+            row.get_text(args.text_dir)
+            row.get_traiter(args.traiter_dir)
+            row.get_openai(args.openai_dir)
+
+            if args.verbose:
+                row.verbose_start()
+
+            row.reconcile(template)
+
+            row.save_traits(args.formatted_dir)
+
+            if args.verbose:
+                row.verbose()
+
+            total_errors += len(row.errors)
+            if total_errors > args.max_errors:
+                logging.error(f"Max errors of {args.max_errors} exceeded")
                 sys.exit(1)
 
-            save_traits(args.formatted_dir, stem, traits)
+        if args.verbose > 1:
+            show_missed(rows)
 
+        logging.info(f"Total errors: {total_errors}")
     log.finished()
 
 
-def build_template():
-    Date()
-    Elevation()
-    Habitat()
-    LatLong()
-
-
-def get_traits(text, stem, r_traits, o_traits) -> tuple[dict[str, Any], int]:
-    print("=" * 80)
-    print(stem)
-    print()
-    print(text)
-    print()
-    pp(r_traits)
-    print()
-    pp(o_traits)
-    print()
-
-    traits = {}
-    errors = 0
-
-    for func in TEMPLATE.reconcile:
-        try:
-            traits |= func(r_traits, o_traits)
-        except ValueError as err:
-            logging.error(f"{err} [{stem}]")
-            errors += 1
-
-    print()
-    pp(traits)
-    print()
-
-    skipped = sorted(set(o_traits.keys()) - set(traits.keys()))
-    logging.info(f"Missed keys {', '.join(skipped)}")
-    print()
-    valid = [k for k in skipped if k in t_dwc.CORE]
-    logging.info(f"Missed valid keys {', '.join(valid)}")
-    print()
-
-    return traits, errors
-
-
-def get_text(text_dir, stem) -> str:
-    path = text_dir / f"{stem}.txt"
-    with open(path) as f:
-        text = f.read()
-    text = t_util.compress(text)
-    return text
-
-
-def get_rule_traits(traiter_dir, stem) -> dict[str, Any]:
-    path = traiter_dir / f"{stem}.json"
-    with open(path) as f:
-        traiter = json.load(f)
-    return traiter
-
-
-def get_openai_traits(openai_dir, stem) -> dict[str, Any]:
-    path = openai_dir / f"{stem}.json"
-    with open(path) as f:
-        openai = json.load(f)
-
-    new = {}
-    for key, value in openai.items():
-        new[key] = value
-        if isinstance(value, dict):
-            new |= {k: v for k, v in value.items()}
-
-    new = {clean_key(k): v for k, v in new.items()}
-    return new
-
-
-def save_traits(parsed_dir, stem, traits):
-    path = parsed_dir / f"{stem}.json"
-    with open(path, "w") as f:
-        json.dump(traits, f)
-
-
 def clean_key(key) -> str:
-    key = key.removeprefix("dc:").removeprefix("dcterms:").removeprefix("dnz:")
-    key = key.removeprefix("dwc-").removeprefix("dwc:").removeprefix("dwc")
-    key = key.removeprefix("Dc:").removeprefix("DwC:")
+    key = key.removeprefix("dcterms:").removeprefix("dnz:").removeprefix("DwC:")
+    key = key.removeprefix("dwc:").removeprefix("dwc").removeprefix("dwc-")
+    key = key.removeprefix("dc:").removeprefix("Dc:")
     key = key.strip(":")
     key = key.strip()
 
@@ -154,11 +207,27 @@ def clean_key(key) -> str:
     return key
 
 
-def count_keys(dir_, stems) -> dict[str, int]:
+def show_missed(rows):
+    missed = defaultdict(int)
+    for row in rows:
+        for m in row.missed:
+            missed[m] += 1
+
+    missed = dict(sorted(missed.items()))
+    print("---- Missed Keys ", "-" * 40)
+    pp(missed)
+    print()
+
+    valid = {k: v for k, v in missed.items() if k in t_dwc.CORE}
+    print("---- Missed DwC Keys ", "-" * 40)
+    pp(valid)
+    print()
+
+
+def count_keys(dir_) -> dict[str, int]:
     keys = defaultdict(int)
 
-    for stem in stems:
-        path = dir_ / f"{stem}.json"
+    for path in dir_.glob("*.json"):
         with open(path) as f:
             obj = json.load(f)
 
@@ -175,8 +244,8 @@ def count_keys(dir_, stems) -> dict[str, int]:
         print(f"{key},{valid},{count}")
         valid_count += 1 if valid else 0
 
-    logging.info(f"{valid_count}/{len(keys)} valid keys")
-    logging.info(f"{len(keys) - valid_count}/{len(keys)} invalid keys")
+    print(f"{valid_count}/{len(keys)} valid keys")
+    print(f"{len(keys) - valid_count}/{len(keys)} invalid keys")
 
     return keys
 
@@ -226,11 +295,20 @@ def parse_args() -> argparse.Namespace:
     )
 
     arg_parser.add_argument(
-        "--errors",
+        "--max-errors",
+        "-e",
         metavar="INT",
         type=int,
         default=999_999_999,
         help="""Allow this many errors before exiting.""",
+    )
+
+    arg_parser.add_argument(
+        "--verbose",
+        "-v",
+        action="count",
+        default=0,
+        help="""Print information for debugging.""",
     )
 
     args = arg_parser.parse_args()
